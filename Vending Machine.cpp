fe05b0e8a1d7daa72ac06806a2046d489c2ed3f3
MurilloLeoni <122821229+MurilloLeoni@users.noreply.github.com>
2023-06-12 23:23:51 -0300

    Update Vending Machine.cpp

--- Vending Machine.cpp
+++ Vending Machine.cpp
@@ -1,4 +1,4 @@
-# Carlos Germano Gasparelho de Souza (RA: 2840482221044) e Murillo Leoni (RA: 2840482221010)
+//Carlos Germano Gasparelho de Souza (RA: 2840482221044) e Murillo Leoni (RA: 2840482221010)
 //Projeto Avaliativo Semestral - Vending Machine
 
 #include <iostream>
